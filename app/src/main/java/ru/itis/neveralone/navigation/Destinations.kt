f9998b636e4d4d174fbe64978c816392755155d0
Iskandar01 <iskandarikhsanov@gmail.com>
2022-03-05 14:23:55 +0300

    NA-12: create splash screen ui

--- app/src/main/java/ru/itis/neveralone/navigation/Destinations.kt
+++ app/src/main/java/ru/itis/neveralone/navigation/Destinations.kt
@@ -6,7 +6,7 @@ package ru.itis.neveralone.navigation
 
 internal sealed class Destination(val key: String) {
     object SplashDestination : Destination("splash")
+    object ChooseLoginMethod : Destination("login_method")
     object SignInDestination : Destination("signin")
     object SignUpDestination : Destination("signup")
 }
-
