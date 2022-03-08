7c9567ab868acfe735b11569cf362be1e24eab8b
iskandar <iskandar200213@gmail.com>
2022-03-08 13:04:36 +0300

    NA-17: fix navigation backstack

--- features/signin/src/main/kotlin/ru/itis/features/signin/SignInScreen.kt
+++ features/signin/src/main/kotlin/ru/itis/features/signin/SignInScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
