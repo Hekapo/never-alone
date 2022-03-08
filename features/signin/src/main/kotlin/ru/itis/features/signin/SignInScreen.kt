1f5c171ce8a1651e93087f6513e7649ef896212c
iskandar <iskandar200213@gmail.com>
2022-03-08 12:56:05 +0300

    NA-17: create custom `ClickableIcon`, create `SignInScreen`

--- features/signin/src/main/kotlin/ru/itis/features/signin/SignInScreen.kt
+++ features/signin/src/main/kotlin/ru/itis/features/signin/SignInScreen.kt
@@ -1,9 +1,12 @@
 @file:OptIn(ExperimentalComposeUiApi::class)
-
 package ru.itis.features.signin
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.platform.SoftwareKeyboardController
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
@@ -12,7 +15,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.SoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
