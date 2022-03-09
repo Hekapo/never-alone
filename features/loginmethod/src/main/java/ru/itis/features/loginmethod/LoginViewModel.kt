6222d1411b0199c3c0c72344ad85ab248cf8b67f
Iskandar01 <iskandarikhsanov@gmail.com>
2022-03-09 14:45:05 +0300

    NA-17: create `LoginViewModel` class

---
+++ features/loginmethod/src/main/java/ru/itis/features/loginmethod/LoginViewModel.kt
@@ -0,0 +1,14 @@
+package ru.itis.features.loginmethod
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Copyright (c) 09.03.2022 Created by Iskandar
+ */
+
+internal data class LoginScreenState(
+    val inputLogin: String
+)
+
+internal class LoginViewModel() : ViewModel() {
+}
