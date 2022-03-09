af0db46627d177b84c1b224d0a7b76474419c3f3
Iskandar01 <iskandarikhsanov@gmail.com>
2022-03-09 15:06:18 +0300

    NA-18: rename `AuthRepository` to `SignInRepository`, create `SignInUseCase`

---
+++ core/domain/src/main/kotlin/ru/itis/core/domain/usecase/SignInUseCase.kt
@@ -0,0 +1,13 @@
+package ru.itis.core.domain.usecase
+
+/**
+ * Copyright (c) 09.03.2022 Created by Iskandar
+ */
+
+
+interface ISignInUseCase {
+    suspend fun logout()
+}
+
+class SignInUseCase {
+}
