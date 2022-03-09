755fb415a5d14e9feb414c9f65cc10d2d371f96f
Iskandar01 <iskandarikhsanov@gmail.com>
2022-03-09 13:37:01 +0300

    NA-17: create custom `TextField`

--- features/signin/src/main/kotlin/ru/itis/features/signin/SignInScreen.kt
+++ features/signin/src/main/kotlin/ru/itis/features/signin/SignInScreen.kt
@@ -1,18 +1,21 @@
 @file:OptIn(ExperimentalComposeUiApi::class)
-
 package ru.itis.features.signin
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.platform.SoftwareKeyboardController
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.SoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
