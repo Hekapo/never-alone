8259f053716aa07aa7a46b9d43a34f1993c0ff78
Iskandar01 <iskandarikhsanov@gmail.com>
2022-03-10 12:09:07 +0300

    NA-17: create `RepositoryModule`

---
+++ core/data/src/main/kotlin/ru/itis/core/data/di/RepositoryModule.kt
@@ -0,0 +1,19 @@
+package ru.itis.core.data.di
+
+import dagger.Binds
+import dagger.Module
+import ru.itis.core.data.repository.SignInRepositoryImpl
+import ru.itis.core.domain.repository.SignInRepository
+
+/**
+ * Copyright (c) 10.03.2022 Created by Iskandar
+ */
+
+@Module(includes = [RepositoryModuleBinds::class])
+class RepositoryModule
+
+internal interface RepositoryModuleBinds {
+    @Binds
+    fun singInRepository(repository: SignInRepositoryImpl): SignInRepository
+
+}
