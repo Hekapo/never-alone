b286aea23c9f66d7d16d9cb366033556de5e4d9e
Iskandar01 <iskandarikhsanov@gmail.com>
2022-03-10 11:54:39 +0300

    NA-17: add `SignInRepositoryImpl`

---
+++ core/data/src/main/kotlin/ru/itis/core/data/repository/SignInRepositoryImpl.kt
@@ -0,0 +1,15 @@
+package ru.itis.core.data.repository
+
+import ru.itis.core.domain.repository.SignInRepository
+import javax.inject.Inject
+
+/**
+ * Copyright (c) 10.03.2022 Created by Iskandar
+ */
+
+
+internal class SignInRepositoryImpl @Inject constructor(
+
+) : SignInRepository {
+
+}
