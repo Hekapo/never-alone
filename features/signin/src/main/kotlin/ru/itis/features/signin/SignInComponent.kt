d40e71b75d33327df159896e4f99a97a9e761f5a
Iskandar01 <iskandarikhsanov@gmail.com>
2022-03-11 11:45:56 +0300

    NA-17: create `SignInDepsProvider`

--- features/signin/src/main/kotlin/ru/itis/features/signin/SignInComponent.kt
+++ features/signin/src/main/kotlin/ru/itis/features/signin/SignInComponent.kt
@@ -1,7 +1,12 @@
 package ru.itis.features.signin
 
+import androidx.annotation.RestrictTo
+import androidx.lifecycle.ViewModel
 import dagger.Component
 import ru.itis.core.Feature
+import ru.itis.core.domain.usecase.ISignInUseCase
+import kotlin.properties.Delegates.notNull
+
 
 /**
  * Created by Iskandar on 11.03.2022.
@@ -16,10 +21,28 @@ internal interface SignInComponent {
         fun deps(signInDeps: SignInDeps): Builder
 
         fun build(): SignInComponent
-
     }
 }
 
 interface SignInDeps {
 
+    val sigInUseCase: ISignInUseCase
+}
+
+interface SignInDepsProvider {
+
+    @get:RestrictTo(RestrictTo.Scope.LIBRARY)
+    val deps: SignInDeps
+
+    companion object : SignInDepsProvider by SignInDepsStore
+}
+
+object SignInDepsStore : SignInDepsProvider {
+
+    override var deps: SignInDeps by notNull()
+}
+
+internal class SignInComponentViewModel : ViewModel() {
+
+    val signInComponent = DaggerSignInComponent.builder().deps(SignInDepsProvider.deps).build()
 }
