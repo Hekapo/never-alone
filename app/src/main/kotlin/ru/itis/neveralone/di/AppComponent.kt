d00c385d62460f96d46aaef34e64e300c7e227ed
iskandar <iskandar200213@gmail.com>
2022-03-12 12:54:44 +0300

    NA-17: add `NetworkModule` to `AppComponent`

--- app/src/main/kotlin/ru/itis/neveralone/di/AppComponent.kt
+++ app/src/main/kotlin/ru/itis/neveralone/di/AppComponent.kt
@@ -3,6 +3,7 @@ package ru.itis.neveralone.di
 import android.app.Application
 import dagger.BindsInstance
 import dagger.Component
+import ru.itis.core.data.di.NetworkModule
 import ru.itis.core.data.di.RepositoryModule
 import ru.itis.core.di.CoroutineModule
 import ru.itis.core.domain.di.UseCaseModule
@@ -14,7 +15,13 @@ import javax.inject.Singleton
  * Copyright (c) 10.03.2022 Created by Iskandar
  */
 
-@[Singleton Component(modules = [RepositoryModule::class, UseCaseModule::class, CoroutineModule::class])]
+@[Singleton Component(
+    modules = [
+        RepositoryModule::class,
+        UseCaseModule::class,
+        CoroutineModule::class,
+        NetworkModule::class]
+)]
 interface AppComponent : SignInDeps {
 
     override val sigInUseCase: ISignInUseCase
