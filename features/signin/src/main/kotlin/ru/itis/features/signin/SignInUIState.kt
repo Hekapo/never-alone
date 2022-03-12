27f5ab68d6592215f9c334c2d631e59e4c21b0fa
iskandar <iskandar200213@gmail.com>
2022-03-12 15:43:56 +0300

    NA-17: remake `SignInUIState`

--- features/signin/src/main/kotlin/ru/itis/features/signin/SignInUIState.kt
+++ features/signin/src/main/kotlin/ru/itis/features/signin/SignInUIState.kt
@@ -4,9 +4,20 @@ package ru.itis.features.signin
  * Created by Iskandar on 12.03.2022.
  */
 
-sealed class SignInUIState {
-    class InputEmail(val email: String = "") : SignInUIState()
-    class InputPassword(val password: String = "") : SignInUIState()
-    class InputPasswordVisibility(val visible: Boolean = false) : SignInUIState()
-    class NetworkError(val error: Boolean = false) : SignInUIState()
+data class SignInUIState(
+    val inputEmail: InputEmailField = InputEmailField(),
+    val inputPassword: InputPasswordField = InputPasswordField(),
+    val networkError: Boolean = false
+) {
+
+    data class InputEmailField(
+        val email: String = "",
+        val isFieldEnabled: Boolean = true,
+    )
+
+    data class InputPasswordField(
+        val password: String = "",
+        val isFieldEnabled: Boolean = true,
+        val isPasswordVisible: Boolean = false
+    )
 }
