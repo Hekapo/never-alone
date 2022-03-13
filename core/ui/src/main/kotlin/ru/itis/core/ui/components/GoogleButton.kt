e315247313c5af28d1641f488a36d53f5c3ea18f
iskandar <iskandar200213@gmail.com>
2022-03-13 12:48:42 +0300

    NA-17: create google signin button

---
+++ core/ui/src/main/kotlin/ru/itis/core/ui/components/GoogleButton.kt
@@ -0,0 +1,93 @@
+package ru.itis.core.ui.components
+
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ru.itis.core.ui.R
+
+
+/**
+ * Created by Iskandar on 13.03.2022.
+ */
+
+@Composable
+fun GoogleButton(
+    modifier: Modifier = Modifier,
+    text: String = "Sign Up with Google",
+    loadingText: String = "Creating Account...",
+    icon: Int = R.drawable.ic_google_logo,
+    shape: Shape = RoundedCornerShape(3.dp),
+    borderColor: Color = Color.LightGray,
+    backgroundColor: Color = MaterialTheme.colors.surface,
+    progressIndicatorColor: Color = MaterialTheme.colors.primary,
+    onClicked: () -> Unit
+) {
+    var clicked by remember { mutableStateOf(false) }
+
+    Surface(
+        modifier = modifier.clickable { clicked = !clicked },
+        shape = shape,
+        border = BorderStroke(width = 1.dp, color = borderColor),
+        color = backgroundColor
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(
+                    start = 12.dp,
+                    end = 16.dp,
+                    top = 12.dp,
+                    bottom = 12.dp
+                )
+                .animateContentSize(
+                    animationSpec = tween(
+                        durationMillis = 300,
+                        easing = LinearOutSlowInEasing
+                    )
+                ),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                painter = painterResource(id = icon),
+                contentDescription = "Google Button",
+                tint = Color.Unspecified
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = if (clicked) loadingText else text)
+            if (clicked) {
+                Spacer(modifier = Modifier.width(16.dp))
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .height(16.dp)
+                        .width(16.dp),
+                    strokeWidth = 2.dp,
+                    color = progressIndicatorColor
+                )
+                onClicked()
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+private fun GoogleButtonPreview() {
+    GoogleButton(
+        text = "Sign Up with Google",
+        loadingText = "Creating Account...",
+        onClicked = {}
+    )
+}
