e6669e25ddb77c4f7485deb56602396300c33fc4
Iskandar01 <iskandarikhsanov@gmail.com>
2022-04-04 14:29:48 +0300

    NA-22: add custom ui-components, complete profile screen

---
+++ core/ui/src/main/kotlin/ru/itis/core/ui/components/ImageTopAppBar.kt
@@ -0,0 +1,93 @@
+package ru.itis.core.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import ru.itis.core.ui.R
+import ru.itis.core.ui.theme.AppTheme
+
+/**
+ * Copyright (c) 04.04.2022 Created by Iskandar
+ */
+
+@Composable
+fun ImageTopAppBar(
+    modifier: Modifier = Modifier,
+    centerImageVector: ImageVector,
+    menuImageVector: ImageVector,
+    onMenuClick: () -> Unit
+) {
+    Surface(elevation = 1.dp) {
+        ConstraintLayout(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(52.dp)
+                .background(AppTheme.colors.backgroundPrimary),
+
+            ) {
+
+            val (icon, menuIcon) = createRefs()
+
+            Box(modifier = Modifier.constrainAs(icon) {
+                top.linkTo(parent.top)
+                bottom.linkTo(parent.bottom)
+                end.linkTo(parent.end)
+                start.linkTo(parent.start)
+            }) {
+                Icon(
+                    imageVector = centerImageVector,
+                    contentDescription = "",
+                    tint = AppTheme.colors.textHighEmphasis
+                )
+            }
+
+            Box(modifier = Modifier.constrainAs(menuIcon) {
+                end.linkTo(parent.end, margin = 8.dp)
+                top.linkTo(parent.top)
+                bottom.linkTo(parent.bottom)
+            }, contentAlignment = Alignment.CenterEnd) {
+                IconButton(
+                    content = {
+                        AppBarIcon(
+                            imageVector = menuImageVector,
+                            contentDescription = "",
+                            tint = AppTheme.colors.textHighEmphasis,
+                        )
+                    },
+                    onClick = onMenuClick
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun AppBarIcon(
+    imageVector: ImageVector,
+    contentDescription: String,
+    tint: Color
+) = Icon(imageVector = imageVector, contentDescription = contentDescription, tint = tint)
+
+@Preview
+@Composable
+fun ImageTopAppBarPreview() {
+    ImageTopAppBar(
+        modifier = Modifier,
+        centerImageVector = ImageVector.vectorResource(id = R.drawable.leaves),
+        menuImageVector = ImageVector.vectorResource(R.drawable.rows),
+        onMenuClick = {}
+    )
+}
--- features/main-screen/src/main/kotlin/ru/itis/main_screen/profile/ProfileScreen.kt
+++ features/main-screen/src/main/kotlin/ru/itis/main_screen/profile/ProfileScreen.kt
@@ -1,21 +1,24 @@
 package ru.itis.main_screen.profile
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Icon
 import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ru.itis.core.ui.R
+import ru.itis.core.ui.components.ImageTopAppBar
 import ru.itis.core.ui.theme.AppTheme
 
 /**
@@ -28,70 +31,77 @@ fun ProfileScreenRoute() {
 }
 
 @Composable
-internal fun ProfileScreen() {
-
-    androidx.compose.material.Surface(
-        color = AppTheme.colors.backgroundPrimary,
+internal fun ProfileScreen(
+    onMenuClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(AppTheme.colors.backgroundPrimary)
     ) {
+        ImageTopAppBar(
+            centerImageVector = ImageVector.vectorResource(id = R.drawable.leaves),
+            menuImageVector = ImageVector.vectorResource(
+                id = R.drawable.rows
+            ),
+            onMenuClick = onMenuClick
+        )
 
-        Column(modifier = Modifier.fillMaxSize()) {
-            TopAppBar(
-                backgroundColor = AppTheme.colors.backgroundPrimary,
-                elevation = 2.dp,
-            ) {
-                Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
-                    Icon(
-                        modifier = Modifier
-                            .height(35.dp)
-                            .width(50.dp),
-                        imageVector = ImageVector.vectorResource(id = R.drawable.leaves),
-                        contentDescription = "",
-                        tint = AppTheme.colors.textHighEmphasis
-                    )
-                }
-            }
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = stringResource(id = R.string.title_profile),
+                style = AppTheme.typography.text28R
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            Image(
+                modifier = Modifier
+                    .size(152.dp)
+                    .clip(CircleShape)
+                    .border(1.dp, Color.Gray, CircleShape),
+                painter = painterResource(id = R.drawable._7163859),
+                contentDescription = "",
 
-            Column(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                Text(text = "Profile", style = AppTheme.typography.text28R)
-                Spacer(modifier = Modifier.height(24.dp))
-                Image(
-                    modifier = Modifier
-                        .size(152.dp)
-                        .clip(CircleShape),
-                    painter = painterResource(id = R.drawable._7163859),
-                    contentDescription = "",
-
-                    )
-                Spacer(modifier = Modifier.height(18.dp))
-                Text(
-                    text = "Name",
-                    style = AppTheme.typography.text20M)
-                Spacer(modifier = Modifier.height(4.dp))
-                Text(
-                    text = "City, Country",
-                    style = AppTheme.typography.text20M,
-                    color = AppTheme.colors.textMediumEmphasis
                 )
-            }
-            Column(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ){
-
-            }
+            Spacer(modifier = Modifier.height(18.dp))
+            Text(
+                text = "Name",
+                style = AppTheme.typography.text20M
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = "City, Country",
+                style = AppTheme.typography.text20M,
+                color = AppTheme.colors.textMediumEmphasis
+            )
+        }
+        Spacer(modifier = Modifier.height(24.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            ProfileRowInfo(
+                title = stringResource(id = R.string.full_name),
+                mainText = "",
+            )
+            ProfileRowInfo(
+                title = stringResource(id = R.string.email_text),
+                mainText = "",
+            )
+            ProfileRowInfo(
+                title = stringResource(id = R.string.social_url),
+                mainText = "",
+            )
         }
     }
-
 }
 
 @Preview
 @Composable
-private fun ProfileScreenPreview() {
-    ProfileScreen()
-
-}
+private fun ProfileScreenPreview() = ProfileScreen(onMenuClick = {})
