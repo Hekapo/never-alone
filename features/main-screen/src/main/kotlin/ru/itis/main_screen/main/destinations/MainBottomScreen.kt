521dae7ba99a3045482756a3a83ab4aa07e0bbf5
Iskandar01 <iskandarikhsanov@gmail.com>
2022-04-18 15:57:50 +0300

    NA-22: migrate to NavController

--- features/main-screen/src/main/kotlin/ru/itis/main_screen/main/destinations/MainBottomScreen.kt
+++ features/main-screen/src/main/kotlin/ru/itis/main_screen/main/destinations/MainBottomScreen.kt
@@ -1,5 +1,6 @@
 package ru.itis.main_screen.main.destinations
 
+import android.content.Context
 import androidx.annotation.StringRes
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Home
@@ -22,4 +23,15 @@ internal sealed class MainBottomScreen(
     object Home : MainBottomScreen("home", R.string.title_home, Icons.Outlined.Home)
     object Messenger : MainBottomScreen("messenger", R.string.title_messenger, Icons.Outlined.Menu)
     object Profile : MainBottomScreen("profile", R.string.title_profile, Icons.Outlined.Person)
+
+
+}
+
+internal fun String.asTitle(context: Context): String {
+    return when (this) {
+        MainBottomScreen.Home.route -> context.getString(MainBottomScreen.Home.resourceId)
+        MainBottomScreen.Messenger.route -> context.getString(MainBottomScreen.Messenger.resourceId)
+        MainBottomScreen.Profile.route -> context.getString(MainBottomScreen.Profile.resourceId)
+        else -> ""
+    }
 }
