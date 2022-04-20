782f3db7bdb6c6d8f91de7412e048a2ba1c41f82
Iskandar01 <iskandarikhsanov@gmail.com>
2022-04-20 16:06:32 +0300

    NA-24: create serch field

---
+++ core/ui/src/main/kotlin/ru/itis/core/ui/components/SearchField.kt
@@ -0,0 +1,12 @@
+package ru.itis.core.ui.components
+
+import androidx.compose.runtime.Composable
+
+/**
+ * Copyright (c) 20.04.2022 Created by Iskandar
+ */
+
+@Composable
+fun SearchField() {
+
+}
