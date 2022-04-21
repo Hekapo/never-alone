9b7d6cde77ac0563cd894bfd9de3d03df57d0a3e
Iskandar01 <iskandarikhsanov@gmail.com>
2022-04-21 16:30:56 +0300

    NA-24: add search field too screen

--- features/settings-screen/src/main/java/com/example/settings_screen/SettingsScreen.kt
+++ features/settings-screen/src/main/java/com/example/settings_screen/SettingsScreen.kt
@@ -1,19 +1,31 @@
+@file:OptIn(ExperimentalComposeUiApi::class)
+
 package com.example.settings_screen
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.platform.SoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import ru.itis.core.ui.R
 import ru.itis.core.ui.components.ImageTopAppBar
+import ru.itis.core.ui.components.LoginTextField
+import ru.itis.core.ui.components.SearchField
 import ru.itis.core.ui.theme.AppTheme
 
 /**
@@ -57,8 +69,61 @@ private fun SettingsScreen() {
                 color = AppTheme.colors.textHighEmphasis
             )
         }
-        Box(modifier = Modifier.weight(1f)) {
+        Spacer(modifier = Modifier.height(12.dp))
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .padding(horizontal = 16.dp)
+        ) {
+            SettingsTopSearchField(
+                inputText = "",
+                onTextChanged = {}
+            )
+        }
+    }
+}
 
+@Composable
+private fun SettingsTopSearchField(
+    inputText: String,
+    keyboardController: SoftwareKeyboardController? = LocalSoftwareKeyboardController.current,
+    onTextChanged: (String) -> Unit
+) {
+    SearchField(onClick = { /*TODO*/ }) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Spacer(modifier = Modifier.width(8.dp))
+            IconButton(onClick = { keyboardController?.hide() }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_search_18),
+                    contentDescription = "",
+                    tint = AppTheme.colors.textLowEmphasis
+                )
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            LoginTextField(
+                inputValue = inputText,
+                placeholder = stringResource(id = R.string.search_settings),
+                onValueChange = onTextChanged,
+                keyboardActions = KeyboardActions { keyboardController?.hide() },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+//                trailingIcon = {
+//                    AnimatedVisibility(
+//                        visible = showCloseIcon,
+//                        enter = scaleIn(),
+//                        exit = scaleOut()
+//                    ) {
+//                        ClickableIcon(
+//                            backgroundColor = AppTheme.colors.backgroundPrimary,
+//                            imageVector = Icons.Default.Close,
+//                            iconTint = AppTheme.colors.buttonPrimary,
+//                            contentDescription = stringResource(id = androidx.compose.foundation.layout.R.string.descriptionIconClose),
+//                            onClick = { onTextChanged("") }
+//                        )
+//                    }
+//                }
+            )
         }
     }
 }
